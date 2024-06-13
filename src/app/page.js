ca530826bb6464725706a9638c847e413872abbd
Imad-Ocreateo <imad@ocreateo.com>
2024-06-13 18:58:15 +0500

    third

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import AboutComponent from "@/components/home/AboutComponent";
 import Banner from "@/components/home/Banner";
+import ExamGuaidComponent from "@/components/home/ExamGuaidComponent";
+import { examdata } from "@/data/homepage";
  
 export default function Home() {
   return (
     <div className="home w-full pagelayout">
      <Banner/>
      <AboutComponent/>
+     <ExamGuaidComponent data={examdata}/>
      
      </div>
   );
