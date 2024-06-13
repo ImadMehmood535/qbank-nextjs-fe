12db492b249ea45568f9dad5a4a444b484847d34
Imad-Ocreateo <imad@ocreateo.com>
2024-06-13 18:55:09 +0500

    about

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
+import AboutComponent from "@/components/home/AboutComponent";
 import Banner from "@/components/home/Banner";
  
 export default function Home() {
   return (
-     <>
+    <div className="home w-full pagelayout">
      <Banner/>
+     <AboutComponent/>
      
-     </>
+     </div>
   );
 }
